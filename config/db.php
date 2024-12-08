0d1c62172d809426c6bb6f0ce2f5e18a4ba04e42
mastermong <mongkon@outlook.com>
2024-12-08 22:40:42 +0700

    Set UTF-8 character encoding for database connection

--- config/db.php
+++ config/db.php
@@ -18,8 +18,11 @@ $username = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];   
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->exec("SET NAMES utf8mb4");
+    $conn->exec("SET CHARACTER SET utf8mb4");
+    $conn->exec("SET CHARACTER_SET_CONNECTION=utf8mb4");
 
 } catch (PDOException $e) {
     die("การเชื่อมต่อฐานข้อมูลล้มเหลว: " . $e->getMessage());
